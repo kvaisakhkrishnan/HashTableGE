19a56a6633623041361ca0f6f58de42fea2ae07f
Vaisakhkrishnan K <cb.en.u4cse20069@cb.students.amrita.edu>
2023-11-30 08:34:58 +0530

    Adding Data

--- src/MyHashMap.java
+++ src/MyHashMap.java
@@ -5,20 +5,17 @@ class MyHashMap<K, V> {
     private int size;
     private MyMapNode<K, V>[] buckets;
 
-    // Constructor
     public MyHashMap(int size) {
         this.size = size;
         this.buckets = new MyMapNode[size];
     }
 
-    // Get the index for a key using hash code
     private int getBucketIndex(K key) {
         int hashCode = key.hashCode();
         int index = hashCode % size;
         return index < 0 ? index + size : index;
     }
 
-    // Get the value for a key
     public V get(K key) {
         int index = getBucketIndex(key);
         MyMapNode<K, V> head = buckets[index];
@@ -30,15 +27,13 @@ class MyHashMap<K, V> {
             head = head.next;
         }
 
-        return null; // Key not found
+        return null; 
     }
 
-    // Put a key-value pair into the hash map
     public void put(K key, V value) {
         int index = getBucketIndex(key);
         MyMapNode<K, V> head = buckets[index];
 
-        // Check if key already exists, update value
         while (head != null) {
             if (head.key.equals(key)) {
                 head.value = value;
@@ -47,13 +42,30 @@ class MyHashMap<K, V> {
             head = head.next;
         }
 
-        // Key not found, add a new node
         MyMapNode<K, V> newNode = new MyMapNode<>(key, value);
         newNode.next = buckets[index];
         buckets[index] = newNode;
     }
 
-    // Display the frequency of words in the hash map
+    public void remove(K key) {
+        int index = getBucketIndex(key);
+        MyMapNode<K, V> head = buckets[index];
+        MyMapNode<K, V> prev = null;
+
+        while (head != null) {
+            if (head.key.equals(key)) {
+                if (prev == null) {
+                    buckets[index] = head.next;
+                } else {
+                    prev.next = head.next;
+                }
+                return;
+            }
+            prev = head;
+            head = head.next;
+        }
+    }
+
     public void displayFrequency() {
         for (MyMapNode<K, V> head : buckets) {
             while (head != null) {
