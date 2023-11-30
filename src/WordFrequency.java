523566840a588bd6211acffed7a18f711d44c9d6
Vaisakhkrishnan K <cb.en.u4cse20069@cb.students.amrita.edu>
2023-11-30 08:31:38 +0530

    Added Large Sentence

--- src/WordFrequency.java
+++ src/WordFrequency.java
@@ -2,14 +2,12 @@ import java.util.StringTokenizer;
 
 public class WordFrequency {
     public static void main(String[] args) {
-        String paragraph = "Paranoids are not paranoid because they are paranoid "
-                + "but because they keep putting themselves deliberately into "
-                + "paranoid avoidable situations";
+        String paragraph = "In the tranquil serenity of the moonlit night, the ethereal glow of the celestial bodies overhead cast a soft radiance upon the idyllic landscape, where the rustling leaves whispered secrets to the gentle breeze, and the distant murmur of a babbling brook wove a melodic tapestry that harmonized with the nocturnal symphony of crickets and owls, creating a mesmerizing and enchanting ambiance that transported the contemplative observer into a realm of sublime wonder and introspective introspection.";
         MyHashMap<String, Integer> wordFrequencyMap = new MyHashMap<>(10);
 
         StringTokenizer tokenizer = new StringTokenizer(paragraph);
         while (tokenizer.hasMoreTokens()) {
-            String word = tokenizer.nextToken().toLowerCase(); // Convert to lowercase for case-insensitive comparison
+            String word = tokenizer.nextToken().toLowerCase(); 
             int frequency = wordFrequencyMap.get(word) == null ? 1 : wordFrequencyMap.get(word) + 1;
             wordFrequencyMap.put(word, frequency);
         }
